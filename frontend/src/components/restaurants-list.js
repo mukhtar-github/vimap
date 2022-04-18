07ef5602731c449b2504a521109b7228362c33ab
Mukhtar <mukhtartanimugarba@gmail.com>
2022-04-18 11:40:56 +0100

    Update changes

---
+++ frontend/src/components/restaurants-list.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function RestaurantsList() {
+  
+  return (
+    <div className='App'>
+      Hello World!
+    </div>
+  )
+}
+
+export default RestaurantsList;
---
+++ frontend/src/components/restaurants.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Restaurant() {
+  
+  return (
+    <div className='App'>
+      Hello World!
+    </div>
+  )
+}
+
+export default Restaurant;
