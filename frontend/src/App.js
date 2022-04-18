98b19c71b85f114d2caedc0b2bf84b6057d6bab1
Mukhtar <mukhtartanimugarba@gmail.com>
2022-04-18 16:53:52 +0100

    Update changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import AddReview from "./components/add-review";
+import Restaurant from "./components/restaurants";
+import RestaurantsList from "./components/restaurants-list";
+import Login from "./components/login";
+
 
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null);
+  }
+  
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/restaurants" className="navbar-brand">
+          Restaurant Reviews
+        </a>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/restaurants"} className="nav-link">
+              Restaurants
+            </Link>
+          </li>
+          <li className="nav-item" >
+            { user ? (
+              <a onClick={logout} className="nav-link" style={{cursor:'pointer'}}>
+                Logout {user.name}
+              </a>
+            ) : (            
+            <Link to={"/login"} className="nav-link">
+              Login
+            </Link>
+            )}
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Routes>
+          <Route exact path={["/", "/restaurants"]} component={RestaurantsList} />
+          <Route 
+            path="/restaurants/:id/review"
+            render={(props) => (
+              <AddReview {...props} user={user} />
+            )}
+          />
+          <Route 
+            path="/restaurants/:id"
+            render={(props) => (
+              <Restaurant {...props} user={user} />
+            )}
+          />
+          <Route 
+            path="/login"
+            render={(props) => (
+              <Login {...props} login={login} />
+            )}
+          />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
