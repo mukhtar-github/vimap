30e500026637cbabf940b3ac0d8a04ff7c83a0c1
Mukhtar <mukhtartanimugarba@gmail.com>
2022-04-19 17:36:16 +0100

    Update file

--- backend/api/vehicles.route.js
+++ backend/api/vehicles.route.js
@@ -1,8 +1,17 @@
 import express from "express"
 import VehiclesCtrl from "./vehicles.controller.js"
+import InfoCtrl from './info.controller.js'
 
 const router = express.Router()
 
 router.route("/").get(VehiclesCtrl.apiGetVehicles)
+router.route("/id/:id").get(VehiclesCtrl.apiGetVehicleById)
+router.route("/models").get(VehiclesCtrl.apiGetVehicleModels)
+
+router
+    .route("/info")
+    .post(InfoCtrl.apiPostInfo)
+    .put(InfoCtrl.apiUpdateInfo)
+    .delete(InfoCtrl.apiDeleteInfo)
 
 export default router
