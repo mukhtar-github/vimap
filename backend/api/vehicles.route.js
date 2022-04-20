a48807e8437ec40ea2828bbfd57fcf0e2cd6e705
Mukhtar <mukhtartanimugarba@gmail.com>
2022-04-20 11:15:30 +0100

    Update files

--- backend/api/vehicles.route.js
+++ backend/api/vehicles.route.js
@@ -9,7 +9,7 @@ router.route("/id/:id").get(VehiclesCtrl.apiGetVehicleById)
 router.route("/models").get(VehiclesCtrl.apiGetVehicleModels)
 
 router
-    .route("/info")
+    .route("/infoUpdate")
     .post(InfoCtrl.apiPostInfo)
     .put(InfoCtrl.apiUpdateInfo)
     .delete(InfoCtrl.apiDeleteInfo)
