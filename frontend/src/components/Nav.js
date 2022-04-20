48c83aabb4d70fc145a481b47c8e30bcc54ceb36
Mukhtar <mukhtartanimugarba@gmail.com>
2022-04-20 08:30:36 +0100

    Update file

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { Transition } from "@headlessui/react";
 
+import AddReview from "./components/add-review";
+import Restaurant from "./restaurant";
+import RestaurantsList from "./components/restaurants-list";
+import Login from "./components/login";
+
 function Nav() {
   const [isOpen, setIsOpen] = useState(false);
   return (
@@ -19,10 +24,10 @@ function Nav() {
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
                   <a
-                    href="#"
+                    href="/restaurant"
                     className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
-                    Dashboard
+                    Vimap
                   </a>
 
                   <a
@@ -111,15 +116,10 @@ function Nav() {
           leaveFrom="opacity-100 scale-100"
           leaveTo="opacity-0 scale-95"
         >
-          {(ref) => (
-            <div className="md:hidden" id="mobile-menu">
-              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                <a
-                  href="#"
-                  className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Dashboard
-                </a>
+        {(ref) => (
+          <div className="md:hidden" id="mobile-menu">
+            <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              <a href="#" className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium">Dashboard</a>
 
                 <a
                   href="#"
@@ -154,20 +154,20 @@ function Nav() {
         </Transition>
       </nav>
 
-      <header className="bg-white shadow">
+      {/* <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
         </div>
-      </header>
-      <main>
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      </header> */}
+      {/* <main>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8"> */}
           {/* <!-- Replace with your content --> */}
-          <div className="px-4 py-6 sm:px-0">
+          {/* <div className="px-4 py-6 sm:px-0">
             <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
-          </div>
+          </div> */}
           {/* <!-- /End replace --> */}
-        </div>
-      </main>
+        {/* </div>
+      </main> */}
     </div>
   );
 }
