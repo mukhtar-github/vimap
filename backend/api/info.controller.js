4f27a6aaa83dc4a30b03d9e6cf2c7de11f6cc37e
Mukhtar <mukhtartanimugarba@gmail.com>
2022-04-20 09:30:24 +0100

    Update file

--- backend/api/info.controller.js
+++ backend/api/info.controller.js
@@ -63,7 +63,7 @@ export default class InfoController {
     static async apiDeleteInfo(req, res, next) {
         try {
             const infoId = req.query.id
-            const userId = req.body.user_id // In production we use authentication method to be sure it's same user.
+            const userId = req.body.user_id // In production we use authentication method to be sure it's same user. We should not include anything in the body of delete request.
             console.log(reviewId)
             const infoResponse = await InfoDAO.deleteInfo(
                 infoId,
