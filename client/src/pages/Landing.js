a415d889725de553a21b7c9b27c936061f3c2792
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-13 04:21:56 +0100

    Update changes

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../assets/images/logo.svg";
-import main from "../assets/images/main.svg";
+import main from "../assets/images/main2.svg";
 
 const Landing = () => {
   return <h4>Landing Page</h4>;
