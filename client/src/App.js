b1de011ddc125628ee3a1bbb9d640a8758fbf98e
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-15 11:35:59 +0100

    Update file

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,11 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
+      <nav>
+        <Link to="/">Dashboard</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/landing">Landing</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<div>Dashboard</div>} />
         <Route path="/register" element={<div>Register</div>} />
