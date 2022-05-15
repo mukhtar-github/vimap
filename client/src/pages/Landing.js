7fbc4c668d1191eb65342e6b601d182d3ec79ee4
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-15 04:52:56 +0100

    Update changes

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -31,11 +31,38 @@ const Landing = () => {
 const Wrapper = styled.main`
   nav {
     width: var(--fliud-width);
-    width: var(--max-width);
+    max-width: var(--max-width);
     margin: 0 auto;
     height: var(--nav-height);
     display: flex;
     align-items: center;
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--primary-600);
+  }
+  .main-img {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
   }
 `;
 
