8a70f5add371b978f5e0e0cc365caa0466613192
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-15 12:31:19 +0100

    Update files

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,15 @@
 import Landing from "./pages/Landing";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Dashboard, Landing, Register, Error } from "./pages";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Dashboard</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/landing">Landing</Link>
-      </nav>
       <Routes>
-        <Route path="/" element={<div>Dashboard</div>} />
-        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
-        <Route path="*" element={<h1>Error</h1>} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
