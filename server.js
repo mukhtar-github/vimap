a3195b3357b9a3ac4bf852ce7cc2db617bcda25d
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-17 17:56:23 +0100

    Update changes

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from "express";
+// Middleware
+import notFoundMiddleware from "./middleware/not-found.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
+
+app.use(notFoundMiddleware);
 
 const port = process.env.PORT || 5000;
 
