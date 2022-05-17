7887563e9edce0b50454f8796b8baa8e192fc718
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-17 15:18:23 +0100

    Update files

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,5 +1,5 @@
 import React, { useReducer, useContext } from "react";
-import { DISPLAY_ALERT } from "./actions";
+import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
 import reducer from "./reducer";
 
 const initialState = {
@@ -15,7 +15,18 @@ const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const displayAlert = () => {
-    dispatch({ type: DISPLAY_ALERT });
+    dispatch({
+      type: DISPLAY_ALERT,
+    });
+    clearAlert();
+  };
+
+  const clearAlert = () => {
+    setTimeout(() => {
+      dispatch({
+        type: CLEAR_ALERT,
+      });
+    }, 3000);
   };
 
   return (
--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,4 +1,4 @@
-import { DISPLAY_ALERT } from "./actions";
+import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
 
 const reducer = (state, action) => {
   if (action.type === DISPLAY_ALERT) {
@@ -9,6 +9,16 @@ const reducer = (state, action) => {
       alertText: "Please provide all values!",
     };
   }
+
+  if (action.type === CLEAR_ALERT) {
+    return {
+      ...state,
+      showAlert: false,
+      alertType: "",
+      alertText: "",
+    };
+  }
+
   throw new Error(`no such action :${action.type}`);
 };
 
