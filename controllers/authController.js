c1f3bdc553ef6eb288129d169e344f444be19ef2
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-21 17:02:37 +0100

    Update file

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,6 +7,11 @@ const register = async (req, res) => {
   if (!name || !email || !password) {
     throw new BadRequestError("Please provide all values");
   }
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email already in use");
+  }
+
   const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
 };
