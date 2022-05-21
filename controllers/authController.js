b28a876739ed53274cd35bf13a7840f243ffdbf3
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-21 06:29:22 +0100

    Update changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,9 @@
 import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
 
-const register = async (req, res, next) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(201).json({ user });
-  } catch (error) {
-    next(error);
-  }
+const register = async (req, res) => {
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
