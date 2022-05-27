8402f6dba904f3c4d2cc93cad0ed09d5b61398bd
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-27 06:17:22 +0100

    Update file

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -44,7 +44,7 @@ const AppProvider = ({ children }) => {
     dispatch({ type: REGISTER_USER_BEGIN });
     try {
       const response = await axios.post("/api/v1/auth/register", currentUser);
-      console.log(response);
+      // console.log(response);
       const { user, token, location } = response.data;
       dispatch({
         type: REGISTER_USER_SUCCESS,
@@ -62,7 +62,7 @@ const AppProvider = ({ children }) => {
       //   location,
       // })
     } catch (error) {
-      console.log(error.response);
+      // console.log(error.response);
       dispatch({
         type: REGISTER_USER_ERROR,
         payload: { msg: error.response.data.msg },
