78343728b123f6775c18e7d36270e457ccb0caad
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-27 04:01:23 +0100

    Update file

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -29,6 +29,30 @@ const reducer = (state, action) => {
     return { ...state, isLoading: true };
   }
 
+  if (action.type === REGISTER_USER_SUCCESS) {
+    return {
+      ...state,
+      user: action.payload.user,
+      token: action.payload.token,
+      userLocation: action.payload.location,
+      vehicleLocation: action.payload.location,
+      isLoading: false,
+      showAlert: true,
+      alertType: "success",
+      alertText: "User Created! Redirecting...",
+    };
+  }
+
+  if (action.type === REGISTER_USER_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: "danger",
+      alertText: action.payload.msg,
+    };
+  }
+
   throw new Error(`no such action :${action.type}`);
 };
 
