eae130abfb8f57d150525b879267872335f1c6f2
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-29 07:40:18 +0100

    Update file

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -31,7 +31,6 @@ const reducer = (state, action) => {
   if (action.type === REGISTER_USER_BEGIN) {
     return { ...state, isLoading: true };
   }
-
   if (action.type === REGISTER_USER_SUCCESS) {
     return {
       ...state,
@@ -45,8 +44,36 @@ const reducer = (state, action) => {
       alertText: "User Created! Redirecting...",
     };
   }
-
   if (action.type === REGISTER_USER_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: "danger",
+      alertText: action.payload.msg,
+    };
+  }
+
+  if (action.type === LOGIN_USER_BEGIN) {
+    return {
+      ...state,
+      isLoading: true,
+    };
+  }
+  if (action.type === LOGIN_USER_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      user: action.payload.user,
+      token: action.payload.token,
+      userLocation: action.payload.location,
+      jobLocation: action.payload.location,
+      showAlert: true,
+      alertType: "success",
+      alertText: "Login Successful! Redirecting...",
+    };
+  }
+  if (action.type === LOGIN_USER_ERROR) {
     return {
       ...state,
       isLoading: false,
