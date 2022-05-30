6aeeca4833b26c8486efffc85055a63945d5b822
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-30 07:21:30 +0100

    Update file

--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
+import { Navbar, SmallSidebar, BigSidebar } from "../../components";
 import Wrapper from "../../assets/wrappers/SharedLayout";
 
 const SharedLayout = () => {
   return (
-    <Wrapper>
-      <nav>
-        <Link to="all-vehicles">all vehicles</Link>
-        <Link to="add-vehicle">add vehicle</Link>
-      </nav>
-      <Outlet />
-    </Wrapper>
+    <>
+      <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className="dashboard-page">
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </>
   );
 };
 
