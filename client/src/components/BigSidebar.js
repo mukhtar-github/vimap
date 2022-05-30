6e8c9bca47f8de4af7918d936b5f11fe973d6b0c
Mukhtar <mukhtartanimugarba@gmail.com>
2022-05-30 07:50:08 +0100

    Update changes

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -4,7 +4,7 @@ import Wrapper from "../assets/wrappers/BigSidebar";
 const BigSidebar = () => {
   return (
     <Wrapper>
-      <h4>Big Side Bar</h4>;
+      <h4>Big Side Bar</h4>
     </Wrapper>
   );
 };
