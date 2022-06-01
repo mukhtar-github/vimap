a9734ea689e906e065f3ea0cc3646c53fb96c8ae
Mukhtar <mukhtartanimugarba@gmail.com>
2022-06-01 17:41:20 +0100

    Update file

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+import links from "../utils/links";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
 
-const SmallSidebar = () => {
+export const SmallSidebar = () => {
   return (
     <Wrapper>
-      <h4>Small Side Bar</h4>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button className="close-btn" onClick={() => console.log("toggle")}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">nav links</div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
