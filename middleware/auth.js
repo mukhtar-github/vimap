1174d2bfe45584f88ea8876183c8c12c17dc19eb
Mukhtar <mukhtartanimugarba@gmail.com>
2022-06-08 04:53:08 +0100

    Update file

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,6 @@
 const auth = async (req, res, next) => {
-  const headers = req.headers;
   const authHeader = req.headers.authorization;
-  console.log(headers);
+
   console.log(authHeader);
   next();
 };
