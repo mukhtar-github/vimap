d05c9d720a455988bc7b2c101c9571a699642a67
Mukhtar <mukhtartanimugarba@gmail.com>
2022-06-09 18:06:49 +0100

    Update files

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,13 @@
+import { UnauthenticatedError } from "../errors/index.js";
+
 const auth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  console.log(authHeader);
+  if (!authHeader) {
+    // why, well is it 400 or 404?
+    // actually 401
+    throw new UnauthenticatedError("Authentication Invalid");
+  }
   next();
 };
 
