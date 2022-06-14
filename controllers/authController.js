215498abb9a8eb0aaba4bd7690d2e5a585cfdb77
Mukhtar <mukhtartanimugarba@gmail.com>
2022-06-14 06:21:43 +0100

    Update files

--- controllers/authController.js
+++ controllers/authController.js
@@ -46,32 +46,30 @@ const login = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
-  const updateUser = async (req, res) => {
-    const { email, name, lastName, location } = req.body;
-    if (!email || !name || !lastName || !location) {
-      throw new BadRequestError("Please provide all values");
-    }
+  const { email, name, lastName, location } = req.body;
+  if (!email || !name || !lastName || !location) {
+    throw new BadRequestError("Please provide all values");
+  }
 
-    const user = await User.findOne({ _id: req.user.userId });
+  const user = await User.findOne({ _id: req.user.userId });
 
-    user.email = email;
-    user.name = name;
-    user.lastName = lastName;
-    user.location = location;
+  user.email = email;
+  user.name = name;
+  user.lastName = lastName;
+  user.location = location;
 
-    await user.save();
+  await user.save();
 
-    // various setups
-    // in this case only id
-    // if other properties included, must re-generate
+  // various setups
+  // in this case only id
+  // if other properties included, must re-generate
 
-    const token = user.createJWT();
-    res.status(StatusCodes.OK).json({
-      user,
-      token,
-      location: user.location,
-    });
-  };
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({
+    user,
+    token,
+    location: user.location,
+  });
 };
 
 export { register, login, updateUser };
