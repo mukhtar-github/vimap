17f6301ea9523ae18a4465aa98c4da5442badf54
Mukhtar <mukhtartanimugarba@gmail.com>
2022-06-17 18:35:02 +0100

    Update file

--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -5,6 +5,13 @@ import { useAppContext } from "../../context/appContext";
 import Wrapper from "../../assets/wrappers/DashboardFormPage";
 
 const Profile = () => {
+  const { user, showAlert, displayAlert, updateUser, isLoading } =
+    useAppContext();
+  const [name, setName] = useState(user?.name);
+  const [email, setEmail] = useState(user?.email);
+  const [lastName, setLastName] = useState(user?.lastName);
+  const [location, setLocation] = useState(user?.location);
+
   return <h1>Profile Page</h1>;
 };
 
