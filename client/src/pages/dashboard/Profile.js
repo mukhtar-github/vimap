64ad8518d9c413f97bfa4953bd61c2cade7a6d8d
Mukhtar <mukhtartanimugarba@gmail.com>
2022-06-17 18:17:28 +0100

    Update file

--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { useState } from "react";
+import { FormRow, Alert } from "../../components";
+import { useAppContext } from "../../context/appContext";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
 
 const Profile = () => {
   return <h1>Profile Page</h1>;
