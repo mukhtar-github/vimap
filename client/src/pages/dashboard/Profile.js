099f5e344c3d5d73fd97e6b268925e707b4d9b53
Mukhtar <mukhtartanimugarba@gmail.com>
2022-06-18 12:41:56 +0100

    Update file

--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -24,30 +24,33 @@ const Profile = () => {
         {showAlert && <Alert />}
         <div className="form-center">
           <FormRow
-                type="text"
-                name="name"
-                value={name}
-                handleChange={(e) => setName(e.target.value)}
-              />
-              <FormRow
-                type="text"
-                labelText="last name"
-                name="lastName"
-                value={lastName}
-                handleChange={(e) => setLastName(e.target.value)}
-              />
-              <FormRow
-                type="email"
-                name="email"
-                value={email}
-                handleChange={(e) => setEmail(e.target.value)}
-              />
-              <FormRow
-                type="text"
-                name="location"
-                value={location}
-                handleChange={(e) => setLocation(e.target.value)}
-              />
+            type="text"
+            name="name"
+            value={name}
+            handleChange={(e) => setName(e.target.value)}
+          />
+          <FormRow
+            type="text"
+            labelText="last name"
+            name="lastName"
+            value={lastName}
+            handleChange={(e) => setLastName(e.target.value)}
+          />
+          <FormRow
+            type="email"
+            name="email"
+            value={email}
+            handleChange={(e) => setEmail(e.target.value)}
+          />
+          <FormRow
+            type="text"
+            name="location"
+            value={location}
+            handleChange={(e) => setLocation(e.target.value)}
+          />
+          <button className="btn btn-block" type="submit" disabled={isLoading}>
+            {isLoading ? "Please Wait..." : "save changes"}
+          </button>
         </div>
       </form>
     </Wrapper>
