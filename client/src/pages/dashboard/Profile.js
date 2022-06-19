ae0977c747fab5330ef9cff0b07a46c7d5ab2445
Mukhtar <mukhtartanimugarba@gmail.com>
2022-06-19 07:36:59 +0100

    Update file

--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -14,10 +14,11 @@ const Profile = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!name || !email || !lastName || !location) {
-      displayAlert();
-      return;
-    }
+    // remove while testing
+    // if (!name || !email || !lastName || !location) {
+    //   displayAlert();
+    //   return;
+    // }
     updateUser({ name, email, lastName, location });
   };
 
