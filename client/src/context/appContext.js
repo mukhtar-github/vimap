c565e71c888bdaf16608636c7d5246647ad2af68
Mukhtar <mukhtartanimugarba@gmail.com>
2022-06-19 08:56:46 +0100

    Update file

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -152,7 +152,20 @@ const AppProvider = ({ children }) => {
   };
 
   const updateUser = async (currentUser) => {
-    console.log(currentUser);
+    try {
+      const { data } = await axios.patch(
+        "/api/v1/auth/updateUser",
+        currentUser,
+        {
+          headers: {
+            Authorization: `Bearer ${state.token}`,
+          },
+        }
+      );
+      console.log(data);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   return (
