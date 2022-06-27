cb4090e005d79d078b89c8259d276c6db5f8a75f
Mukhtar <mukhtartanimugarba@gmail.com>
2022-06-27 13:51:16 +0100

    Update file

--- README.md
+++ README.md
@@ -3111,14 +3111,16 @@ const VehicleSchema = new mongoose.Schema(
       required: [true, "Please provide vehicle make"],
       maxlength: 50,
     },
-    model: {
-      type: String,
-      required: [true, "Please provide vehicle model"],
-      maxlength: 100,
-    },
     year: {
       type: String,
       enum: [
+        "2003",
+        "2004",
+        "2005",
+        "2006",
+        "2007",
+        "2008",
+        "2009",
         "2010",
         "2011",
         "2012",
@@ -3137,16 +3139,35 @@ const VehicleSchema = new mongoose.Schema(
       ],
       default: "2010",
     },
-
-    jobType: {
+    registration: {
       type: String,
-      enum: ["full-time", "part-time", "remote", "internship"],
-      default: "full-time",
+      required: [true, "Please provide registration"],
+      maxlength: 50,
     },
-    jobLocation: {
+    chassisNumber: {
       type: String,
-      default: "my city",
+      default: "chassis",
       required: true,
+    },
+    rimSize: {
+      type: String,
+      enum: [
+        "195-R15C",
+        "205/70/R15",
+        "205-R16C",
+        "215/55/R17",
+        "265/65/R17",
+        "235/45/R18",
+        "265/60/R18",
+        "285/50/R20",
+        "700R16",
+      ],
+      default: "700R16",
+    },
+    insuranceDate: {
+      type: String,
+      required: [true, "Please provide Insurance date"],
+      maxlength: 100,
     },
     createdBy: {
       type: mongoose.Types.ObjectId,
@@ -3157,7 +3178,7 @@ const VehicleSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model("Job", JobSchema);
+export default mongoose.model("Job", VehicleSchema);
 ```
 
 #### Create Job
--- models/Vehicle.js
+++ models/Vehicle.js
@@ -1,35 +1,77 @@
 import mongoose from "mongoose";
 
-const VehicleSchema = new mongoose.Schema({
-  make: {
-    type: String,
-    required: [true, "Please provide vehicle make"],
-    maxlength: 50,
+const VehicleSchema = new mongoose.Schema(
+  {
+    make: {
+      type: String,
+      required: [true, "Please provide vehicle make"],
+      maxlength: 50,
+    },
+    year: {
+      type: String,
+      enum: [
+        "2003",
+        "2004",
+        "2005",
+        "2006",
+        "2007",
+        "2008",
+        "2009",
+        "2010",
+        "2011",
+        "2012",
+        "2013",
+        "2014",
+        "2015",
+        "2016",
+        "2017",
+        "2018",
+        "2019",
+        "2020",
+        "2021",
+        "2022",
+        "2023",
+        "2024",
+      ],
+      default: "2010",
+    },
+    registration: {
+      type: String,
+      required: [true, "Please provide registration"],
+      maxlength: 50,
+    },
+    chassisNumber: {
+      type: String,
+      default: "chassis",
+      required: true,
+    },
+    rimSize: {
+      type: String,
+      enum: [
+        "195-R15C",
+        "205/70/R15",
+        "205-R16C",
+        "215/55/R17",
+        "265/65/R17",
+        "235/45/R18",
+        "265/60/R18",
+        "285/50/R20",
+        "700R16",
+      ],
+      default: "700R16",
+    },
+    insuranceDate: {
+      type: String,
+      required: [true, "Please provide Insurance date"],
+      maxlength: 100,
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user"],
+    },
   },
-  model: {
-    type: String,
-    required: [true, "Please provide vehicle model"],
-    maxlength: 100,
-  },
-  year: {
-    type: String,
-    enum: [
-      "2010",
-      "2011",
-      "2012",
-      "2013",
-      "2014",
-      "2015",
-      "2016",
-      "2017",
-      "2018",
-      "2019",
-      "2020",
-      "2021",
-      "2022",
-      "2023",
-      "2024",
-    ],
-    default: "2010",
-  },
-});
+  { timestamps: true }
+);
+
+export default mongoose.model("Job", VehicleSchema);
