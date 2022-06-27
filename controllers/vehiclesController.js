2971630fb3848313fdce7fcbf43b15813696e601
Mukhtar <mukhtartanimugarba@gmail.com>
2022-06-27 15:38:13 +0100

    Update file

--- controllers/vehiclesController.js
+++ controllers/vehiclesController.js
@@ -1,5 +1,25 @@
+import Vehicle from "../models/Vehicle.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, NotFoundError } from "../errors/index.js";
+
 const createVehicle = async (req, res) => {
-  return res.send("create vehicle");
+  const { make, registration, chassisNumber, insuranceDate, attachedTo } =
+    req.body;
+
+  if (
+    !make ||
+    !registration ||
+    !chassisNumber ||
+    !insuranceDate ||
+    !attachedTo
+  ) {
+    throw new BadRequestError("Please Provide All Values");
+  }
+
+  req.body.createdBy = req.user.userId;
+
+  const vehicle = await Vehicle.create(req.body);
+  res.status(StatusCodes.CREATED).json({ vehicle });
 };
 
 const deleteVehicle = async (req, res) => {
