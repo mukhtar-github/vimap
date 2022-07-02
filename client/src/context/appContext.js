c830a39dc4c87250b7528d5f21cd48fe4488308c
Mukhtar <mukhtartanimugarba@gmail.com>
2022-07-02 10:30:35 +0100

    Update files

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -12,6 +12,7 @@ import {
   UPDATE_USER_BEGIN,
   UPDATE_USER_SUCCESS,
   UPDATE_USER_ERROR,
+  HANDLE_CHANGE,
 } from "./actions";
 
 const token = localStorage.getItem("token");
@@ -191,6 +192,13 @@ const AppProvider = ({ children }) => {
     clearAlert();
   };
 
+  const handleChange = ({ name, value }) => {
+    dispatch({
+      type: HANDLE_CHANGE,
+      payload: { name, value },
+    });
+  };
+
   return (
     <AppContext.Provider
       value={{
@@ -200,6 +208,7 @@ const AppProvider = ({ children }) => {
         toggleSidebar,
         logoutUser,
         updateUser,
+        handleChange,
       }}
     >
       {children}
--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -9,6 +9,7 @@ import {
   UPDATE_USER_BEGIN,
   UPDATE_USER_SUCCESS,
   UPDATE_USER_ERROR,
+  HANDLE_CHANGE,
 } from "./actions";
 
 import { initialState } from "./appContext";
@@ -96,6 +97,10 @@ const reducer = (state, action) => {
       alertType: "danger",
       alertText: action.payload.msg,
     };
+  }
+
+  if (action.type === HANDLE_CHANGE) {
+    return { ...state, [action.payload.name]: action.payload.value };
   }
   throw new Error(`no such action :${action.type}`);
 };
