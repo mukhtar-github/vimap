58b03ab9307dd0e0198882e65615f24017ad3fe9
Mukhtar <mukhtartanimugarba@gmail.com>
2022-07-14 08:18:01 +0100

    Update file

--- client/src/components/VehiclesContainer.js
+++ client/src/components/VehiclesContainer.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppContext } from "../context/appContext";
+import Loading from "./Loading";
+import Vehicle from "./Vehicle";
+import Wrapper from "../assets/wrappers/VehiclesContainer";
 
 const VehiclesContainer = () => {
-  return <h2>vehicles container</h2>;
+  const { getVehicles, vehicles, isLoading, page, totalVehicles } =
+    useAppContext();
+  useEffect(() => {
+    getVehicles();
+  }, []);
+
+  if (isLoading) {
+    return <Loading center />;
+  }
+  if (vehicles.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No vehicles to display...</h2>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <h5>
+        {totalVehicles} vehicle{vehicles.length > 1 && "s"} found
+      </h5>
+      <div className="vehicles">
+        {vehicles.map((vehicle) => {
+          return <Vehicle key={vehicle._id} {...vehicle} />;
+        })}
+      </div>
+    </Wrapper>
+  );
 };
 
 export default VehiclesContainer;
