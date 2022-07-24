c237d35ec2836039ef3db4119ac1e336862411c3
Mukhtar <mukhtartanimugarba@gmail.com>
2022-07-24 11:15:28 +0100

    Update file

--- client/src/components/Vehicle.js
+++ client/src/components/Vehicle.js
@@ -1,5 +1,10 @@
 import React from "react";
 import moment from "moment";
+import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useAppContext } from "../context/appContext";
+import Wrapper from "../assets/wrappers/Vehicle";
+import VehicleInfo from "./VehicleInfo";
 
 const Vehicle = ({ make, createdAt }) => {
   let date = moment(createdAt);
