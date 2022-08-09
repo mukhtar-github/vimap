e151efcb9d0c8857affcf44838978cbf70216805
Mukhtar <mukhtartanimugarba@gmail.com>
2022-08-09 17:39:16 +0100

    Update file

--- client/src/pages/dashboard/Stats.js
+++ client/src/pages/dashboard/Stats.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useEffect } from "react";
+import { useAppContext } from "../../context/appContext";
+import { StatsContainer, Loading, ChartsContainer } from "../../components";
 
 const Stats = () => {
-  return <h1>Stats Page</h1>;
+  const { showStats, isLoading, monthlyUpdates } = useAppContext();
+  useEffect(() => {
+    showStats();
+  }, []);
+
+  if (isLoading) {
+    return <Loading center />;
+  }
+
+  return (
+    <>
+      <StatsContainer />
+      {monthlyUpdates.length > 0 && <ChartsContainer />}
+    </>
+  );
 };
 
 export default Stats;
