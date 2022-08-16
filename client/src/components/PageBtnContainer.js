5dc25c4d7a0e2139ea3b14f60fe36e53c27ff32c
Mukhtar <mukhtartanimugarba@gmail.com>
2022-08-16 10:04:52 +0100

    Update file

--- client/src/components/PageBtnContainer.js
+++ client/src/components/PageBtnContainer.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { useAppContext } from "../context/appContext";
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
+import Wrapper from "../assets/wrappers/PageBtnContainer";
 
-const PageBtnContainer = () => {
-  return <div></div>;
+const PageButtonContainer = () => {
+  const { numOfPages, page } = useAppContext();
+
+  const prevPage = () => {
+    console.log("prev page");
+  };
+  const nextPage = () => {
+    console.log("next page");
+  };
+
+  return (
+    <Wrapper>
+      <button className="prev-btn" onClick={prevPage}>
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+
+      <div className="btn-container">buttons</div>
+
+      <button className="next-btn" onClick={nextPage}>
+        next
+        <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
+  );
 };
 
-export default PageBtnContainer;
+export default PageButtonContainer;
