9e7f97668a46c20b7e18d304c428c35bd8d99c89
Mukhtar <mukhtartanimugarba@gmail.com>
2022-08-18 10:45:28 +0100

    Update file

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 import express from "express";
+const app = express();
 import dotenv from "dotenv";
+dotenv.config();
 import "express-async-errors";
 import morgan from "morgan";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
 // db and authenticateUser
 import connectDB from "./db/connect.js";
@@ -15,29 +21,31 @@ import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import authenticateUser from "./middleware/auth.js";
 
-const app = express();
-
-dotenv.config();
-
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome!" });
-});
-
-app.get("/api/v1", (req, res) => {
-  res.json({ msg: "API" });
-});
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/vehicles", authenticateUser, vehiclesRouter);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
+
+// app.get("/", (req, res) => {
+//   res.json({ msg: "Welcome!" });
+// });
+// app.get("/api/v1", (req, res) => {
+//   res.json({ msg: "API" });
+// });
 
 const port = process.env.PORT || 5001;
 
