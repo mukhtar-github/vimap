1adac18e46ab4b310d5a0166c39f640d4e86aa6d
Mukhtar <mukhtartanimugarba@gmail.com>
2022-08-18 11:22:59 +0100

    Update files

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import path from "path";
 
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+
 // db and authenticateUser
 import connectDB from "./db/connect.js";
 
@@ -27,12 +31,18 @@ if (process.env.NODE_ENV !== "production") {
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// only when ready to deploy
 app.use(express.static(path.resolve(__dirname, "./client/build")));
+
 app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/vehicles", authenticateUser, vehiclesRouter);
 
+// only when ready to deploy
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
