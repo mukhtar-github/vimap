de196b9d302d361fc94142b3ebff99fd18da36a0
Mukhtartg <mukhtartanimugarba@gmail.com>
2022-11-17 15:05:25 +0100

    Update Landing.js

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -21,9 +21,11 @@ const Landing = () => {
             challenge of going through archives to get vehicle information for
             updates which was time-consuming. The challenge gave birth to the
             idea of VIMAP, an acronym for Vehicles Information Management
-            Application. This software provides a solution to replace the hard
-            copies method of accessing, saving, and updating vehicle
-            information. VIMAP is a fleet wallet for organizations. The
+            Application. The software provides a solution to replace the analog
+            method of accessing, saving, and updating vehicle information. [The
+            Vimap
+            Manual](https://medium.com/@mukhtartanimugarba/the-vimap-manual-bb9d299cebba)
+            will guide you through creating a fleet of vehicles data bank. The
             software's features are the Collection and Storage of Vehicles
             Information, Search, Updates, Graphical Data Visualization, and
             more.
